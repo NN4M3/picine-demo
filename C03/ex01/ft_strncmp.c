417df3ec6d0c12b01a1eb2719d94664f8442b882
NN4M3 <saad.noob123@gmail.com>
2025-01-13 16:42:51 +0100

    ex01 & ex02

--- C03/ex01/ft_strncmp.c
+++ C03/ex01/ft_strncmp.c
@@ -1,1 +1,20 @@
-// ??
+#include <unistd.h>
+
+int ft_strncmp(char *s1, char *s2, unsigned int n){
+	int i;
+	i = 0;
+
+	while((s1 != '\0' || s2 != '\0') && i < n)
+	{
+		if(s1[i] > s2[i])
+		{
+			return 1;
+		} else if (s1[i] < s2[i])
+		{
+			return -1;
+		}
+		i++;
+	}
+
+	return 0;
+}
